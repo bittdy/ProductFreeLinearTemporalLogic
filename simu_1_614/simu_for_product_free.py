7eaf8a3985e591307ee5cdb1dcb2fd942e76bad1
bittdy <499801924@qq.com>
2018-10-10 13:45:07 +0800

    simulat

---
+++ simu_1_614/simu_for_product_free.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 10 21:22:43 2018
+
+@author: bittdy
+"""
+
+#at first: change previous code, add the request attribute to the plan result
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#class simulator(object):
+#    def __init__(self,agents_plan_lists):
+#        #agent_plan_lists:[robot1:[[prefix],[suffix]],[],[]]
+#        self.plan_lists = agents_plan_lists
+#        
+#    def simulate(self):
+#        agent_number = len(self.plan_lists)
+        
+
+
+plt.grid(linewidth = 5)
+plt.show()
