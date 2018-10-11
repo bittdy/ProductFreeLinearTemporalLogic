beec2277a09a54527eb63c9002cb3f0ea831fe44
bittdy <499801924@qq.com>
2018-10-11 10:48:56 +0800

    animation

--- simu_1_614/simu_for_product_free.py
+++ simu_1_614/simu_for_product_free.py
@@ -8,18 +8,29 @@ Created on Wed Oct 10 21:22:43 2018
 
 #at first: change previous code, add the request attribute to the plan result
 
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation  # 动图的核心函数
 
-#class simulator(object):
-#    def __init__(self,agents_plan_lists):
-#        #agent_plan_lists:[robot1:[[prefix],[suffix]],[],[]]
-#        self.plan_lists = agents_plan_lists
-#        
-#    def simulate(self):
-#        agent_number = len(self.plan_lists)
-        
+# 创建画布
+fig, ax = plt.subplots()
 
 
-plt.grid(linewidth = 5)
-plt.show()
+# 画出一个维持不变（不会被重画）的散点图和一开始的那条直线。
+x = np.arange(0, 20, 0.1)
+ax.scatter(x, x + np.random.normal(0, 3.0, len(x)))
+line, = ax.plot(x, x - 5, 'r-', linewidth=2)
+
+def update(i):
+    label = 'timestep {0}'.format(i)
+    print(label)
+    # 更新直线和x轴（用一个新的x轴的标签）。
+    # 用元组（Tuple）的形式返回在这一帧要被重新绘图的物体
+    line.set_ydata(x - 5 + i)  # 这里是重点，更新y轴的数据
+    ax.set_xlabel(label)    # 这里是重点，更新x轴的标签
+    return line, ax
+
+# FuncAnimation 会在每一帧都调用“update” 函数。
+# 在这里设置一个10帧的动画，每帧之间间隔200毫秒
+anim = FuncAnimation(fig, update, frames=np.arange(0, 10), interval=200)
+
